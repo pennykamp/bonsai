93b4d50cbf3afab90005f8e99c809bc72aab9724
pennykamp <pennykampili@yahoo.com>
2021-07-04 23:08:16 +0300

    conv data

--- sandbox/convert_data.sh
+++ sandbox/convert_data.sh
@@ -2,28 +2,28 @@
 
 #! /bin/zsh
 
-datasets=('bonsai-master/sandbox/data/eurlex')
+datasets=('bonsai-master/sandbox/data/rapt')
 
 FULL=bonsai-master/tools
 
 for dataset in ${datasets}; do
-    echo ${dataset}
     dir=${dataset}
 
     echo ${dir}
 
     if [ ! -f ${dir}/trn_X_Y.txt ]; then
     echo "kati"
+    echo  ${dir}/train_bow.txt
 	perl ${FULL}/convert_format.pl\
-	     ${dataset}/eurlex_train.txt\
+	     ${dir}/train_bow.txt\
 	     ${dir}/trn_X_Xf.txt\
 	     ${dir}/trn_X_Y.txt
     fi
     echo ${FULL}/convert_format.pl
     if [ ! -f ${dir}/tst_X_Y.txt ]; then  
-    echo ${FULL}/convert_format.pl 
+    # echo ${FULL}/convert_format.pl 
 	perl ${FULL}/convert_format.pl\
-	     ${dataset}/eurlex_test.txt\
+	     ${dir}/test_bow.txt\
 	     ${dir}/tst_X_Xf.txt\
 	     ${dir}/tst_X_Y.txt
     fi
