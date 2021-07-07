97723088f682f686aebec9ae4436a718594e3502
pennykamp <pennykampili@yahoo.com>
2021-07-07 21:51:27 +0300

    sumplerun example w=50

--- shallow/sample_run.sh
+++ shallow/sample_run.sh
@@ -20,11 +20,28 @@ mkdir -p $model_dir
 # NOTE: The usage of Bonsai for other datasets requires setting parameter `-m` to 2 for smaller datasets like EUR-Lex, Wikipedia-31K 
 #       and to 3 for larger datasets like Delicious-200K, WikiLSHTC-325K, Amazon-670K, Wikipedia-500K, Amazon-3M.
 
+# ./bonsai_train $trn_ft_file $trn_lbl_file $trn_ft_lbl_file $model_dir \
+#     -T 3 \
+#     -s 0 \
+#     -t 3 \
+#     -w 100 \
+#     -b 1.0 \
+#     -c 1.0 \
+#     -m 2 \
+#     -f 0.1 \
+#     -fcent 0 \
+#     -k 0.0001 \
+#     -siter 20 \
+#     -q 0 \
+#     -ptype 0 \
+#     -ctype 0
+
+
 ./bonsai_train $trn_ft_file $trn_lbl_file $trn_ft_lbl_file $model_dir \
     -T 3 \
     -s 0 \
     -t 3 \
-    -w 100 \
+    -w 50 \
     -b 1.0 \
     -c 1.0 \
     -m 2 \
@@ -35,6 +52,7 @@ mkdir -p $model_dir
     -q 0 \
     -ptype 0 \
     -ctype 0
+
 
 # testing
 # Reads test features (in $tst_ft_file), FastXML model (in $model_dir), and writes test label scores to $score_file
